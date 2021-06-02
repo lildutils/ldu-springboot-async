e20b18b7664c53bdbb6a1466da4ada87eded352f
lildworks <lildworks@gmail.com>
2021-06-02 19:15:53 +0200

    FEAT: Enable LDuAsync

---
+++ src/main/java/hu/lildutils/springboot/async/EnableLDuAsync.java
@@ -0,0 +1,19 @@
+package hu.lildutils.springboot.async;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Import;
+
+import hu.lildutils.springboot.async.config.LDuAsyncConfigurer;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Import(LDuAsyncConfigurer.class)
+public @interface EnableLDuAsync {
+
+}
